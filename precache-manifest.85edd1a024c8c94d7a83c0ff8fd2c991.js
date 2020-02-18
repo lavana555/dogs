5cf62af1e76eb9cac848655d4c94ac0d846457df
lavana555 <timofeevniki3@gmail.com>
2020-02-18 13:01:46 +0300

    Updates

---
+++ precache-manifest.85edd1a024c8c94d7a83c0ff8fd2c991.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbb8ec6e38635a05f15b736a05840b7",
+    "url": "/dogs/index.html"
+  },
+  {
+    "revision": "371ded13ee5b3216541a",
+    "url": "/dogs/static/css/main.07618f91.chunk.css"
+  },
+  {
+    "revision": "ae5d175a37e50ca1a33d",
+    "url": "/dogs/static/js/2.e9578310.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/dogs/static/js/2.e9578310.chunk.js.LICENSE"
+  },
+  {
+    "revision": "371ded13ee5b3216541a",
+    "url": "/dogs/static/js/main.c6eb0343.chunk.js"
+  },
+  {
+    "revision": "e7bc7391b92d1826d039",
+    "url": "/dogs/static/js/runtime-main.2691e19f.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/dogs/static/media/dog.75fa68d4.mp3"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dogs/static/media/logo.5d5d9eef.svg"
+  }
+]);
